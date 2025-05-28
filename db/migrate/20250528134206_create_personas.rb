971880f6544fa803de5718b34c17291320d7b4a2
Rei <chloerei@gmail.com>
2025-05-28 21:52:02 +0800

    Add persona model

---
+++ db/migrate/20250528134206_create_personas.rb
@@ -0,0 +1,12 @@
+class CreatePersonas < ActiveRecord::Migration[8.0]
+  def change
+    create_table :personas do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.string :name, null: false
+      t.text :description, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_27_123831) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_28_134206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "personas", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_personas_on_user_id"
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -32,5 +41,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_27_123831) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "personas", "users"
   add_foreign_key "sessions", "users"
 end
