833931eccdcc375b7206df010c1bef66aaea723c
Rei <chloerei@gmail.com>
2025-07-11 15:50:22 +0800

    edit room index style

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,6 +3,6 @@ class Character < ApplicationRecord
   has_many :members, dependent: :destroy
 
   has_one_attached :avatar do |attachable|
-    attachable.variant :thumb, resize_to_limit: [ 160, 160 ]
+    attachable.variant :thumb, resize_to_limit: [ 512, 512 ]
   end
 end
