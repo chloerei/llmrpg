fbab4c3db5b2c3a67fcf870928cec7a0228a4120
Rei <chloerei@gmail.com>
2025-07-12 16:56:53 +0800

    edit sign up layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   include Authentication
   include Pagy::Backend
 
+  layout "app"
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
