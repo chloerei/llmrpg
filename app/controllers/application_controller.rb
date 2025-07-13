38dad3aeeff8d9f58e0b5187279f7aa25c8fabd9
Rei <chloerei@gmail.com>
2025-07-13 20:40:41 +0800

    Add dock nav for mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,5 @@ class ApplicationController < ActionController::Base
   layout "app"
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
 end
