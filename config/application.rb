5f66b26b9a595f2264bfe139873c639623f5b340
Rei <chloerei@gmail.com>
2025-07-14 21:55:01 +0800

    add zh-CN locale

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module Roleplay
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.i18n.available_locales = %i[ en zh-CN ]
+    config.i18n.default_locale = :en
+
     # Don't generate system test files.
     config.generators.system_tests = nil
 
