824fa79fa3f5a630c7ea627df4d74eae6d92d721
Rei <chloerei@gmail.com>
2025-08-01 15:56:46 +0800

    fix avatar path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def character_avatar_url(character)
+    if character.avatar.attached?
+      url_for(character.avatar.variant(:thumb))
+    else
+      asset_path("icon.png")
+    end
+  end
 end
